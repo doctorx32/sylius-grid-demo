ed5df62b54ebc8c9e7781c80581d0b6957fa5852
Dmitry Chornobyl <dch@inbox.lv>
2019-02-05 22:29:25 +0200

    added dummy entity Supplier, added default grid actions & fields templates, added configs for Grid, Resource bundles

---
+++ src/Entity/Supplier.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 19.5.2
+ * Time: 21:10
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+/**
+ * @ORM\Entity
+ */
+class Supplier implements ResourceInterface
+{
+    public const TYPE_NOVICE = 0;
+    public const TYPE_AVERAGE = 1;
+    public const TYPE_BOSS = 2;
+    /**
+     * @var int
+     *
+     * @ORM\Id()
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $surname;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="smallint")
+     */
+    private $type;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param string $surname
+     */
+    public function setSurname(string $surname): void
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return int
+     */
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param int $type
+     */
+    public function setType(int $type): void
+    {
+        $this->type = $type;
+    }
+}
