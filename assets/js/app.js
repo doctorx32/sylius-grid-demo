3f1cf7c73f8a50dfb0d3250bac97074befd22c2b
Dmitry Chornobyl <dch@inbox.lv>
2019-09-16 22:39:18 +0300

    vue-bootstrap added

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,12 +7,16 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 import Vue from "vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "../css/app.css";
 import Create from "./create.vue";
+import BootstrapVue from 'bootstrap-vue'
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
+Vue.use(BootstrapVue);
 new Vue({
     el: '#app',
     template: '<Create/>',
